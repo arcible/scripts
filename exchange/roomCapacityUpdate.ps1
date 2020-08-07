f8a220666ea9842c732d6a3c35957fcbfe6e777b
Richard J Green <richardjgreen@users.noreply.github.com>
2020-08-07 11:59:29 +0100

    Update roomCapacityUpdate.ps1

--- exchange/roomCapacityUpdate.ps1
+++ exchange/roomCapacityUpdate.ps1
@@ -4,8 +4,8 @@ $rooms = Get-Mailbox -RecipientTypeDetails RoomMailbox
 Write-Host -ForegroundColor Yellow "The capacity factor is set to $capacityFactor. All rooms will have their capacity divided by this number."
 
 ForEach ($room in $rooms) {
-  $roomCount = $roomCount+1
-  Write-Progress -Activity "Setting New Room Capacity" -Status "Progress:" -PercentComplete ($roomCount/$rooms.Count*100)
+  $roomCount = $roomCount + 1
+  Write-Progress -Activity "Setting New Room Capacity" -Status "Progress:" -PercentComplete ($roomCount / $rooms.Count*100)
 
   Write-Host "Room $($room.Name) capacity will be changed from $($room.ResourceCapacity) to $newCapacity."
 
